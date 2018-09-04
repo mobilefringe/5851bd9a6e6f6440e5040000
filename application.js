636fb7bf578addb828109a9fe6f269dad84ca329
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-04 17:31:03 -0400

    Spectrum Square 1.1  /application.js

--- application.js
+++ application.js
@@ -596,8 +596,7 @@ function load_map(reg, stores){
     map = $('#mapsvg_store_list').mapSvg({
         source: getSVGMapURL(),    // Path to SVG map
         colors: {stroke: '#aaaaaa', selected: -20, hover: "#a6a6a6"},
-        height:400,
-        width:'100%',
+        height:600,
         disableAll: true,
         regions: reg,
         tooltipsMode: 'custom',
